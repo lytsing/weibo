9cfa77aafad4ee32bb63d45bb3849bf0fa0ff5f2
Lytsing Huang <hlqing@gmail.com>
2013-05-25 15:56:35 +0800

    delete actionbar pulltorefresh-and-loadmore projects, they don't need to put into git, just download them from github.

--- weibo/build.sh
+++ weibo/build.sh
@@ -5,10 +5,10 @@
 # Date: 2011-12-22
 #
 
-cd ../pulltorefresh-and-loadmore/
+cd ../../pulltorefresh-and-loadmore/
 android update project -p . -t android-8
 cd -
-cd ../actionbar/
+cd ../../actionbar/
 android update project -p . -t android-16
 cd -
 
