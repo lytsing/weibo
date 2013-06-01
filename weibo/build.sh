da4610c9972d1fb78e5ff8afcc1ed3c1af50dcea
Lytsing Huang <hlqing@gmail.com>
2013-06-01 14:27:07 +0800

    modify build.sh for release build, but has problem use weibo sdk in proguard.cfg, will fix it on later.

--- weibo/build.sh
+++ weibo/build.sh
@@ -5,6 +5,16 @@
 # Date: 2011-12-22
 #
 
+if [ $# != 1 ]; then
+	echo "Usage: $0 [debug|release]";
+	exit;
+fi
+
+PRJ=Weibo
+DATE=`date +%Y%m%d`
+VER=1.0
+NAME=$PRJ-$DATE-$VER
+
 cd ../../pulltorefresh-and-loadmore/
 android update project -p . -t android-8
 cd -
@@ -17,9 +27,7 @@ cd ../../abs
 android update project -p . -t android-16
 cd -
 
-android update project -p . --name Weibo -t android-16
-ant debug
+android update project --path . --name $NAME -t android-16
 
-# Install 
-adb install -r bin/Weibo-debug.apk 
+ant $1
 
